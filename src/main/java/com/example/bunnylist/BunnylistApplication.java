d78950c983823d81089d4799f6edc80bb9c40a23
Dan <il0214li@daum.net>
2020-04-01 11:48:34 +0900

    initialized

---
+++ src/main/java/com/example/bunnylist/BunnylistApplication.java
@@ -0,0 +1,13 @@
+package com.example.bunnylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunnylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunnylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bunnylist/BunnylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bunnylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunnylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

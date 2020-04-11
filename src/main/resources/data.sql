b0831203c4e99876f4dfecb629bfe11b3190ee02
DannKim57 <il0214li@daum.net>
2020-04-11 23:58:00 +0900

    Tested methods in CarrotRepository and BunnyRepository

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,7 @@ INSERT INTO types VALUES (3, 'lizard');
 INSERT INTO types VALUES (4, 'snake');
 INSERT INTO types VALUES (5, 'bird');
 INSERT INTO types VALUES (6, 'hamster');
+
+
+INSERT INTO bunnies VALUES (1, '2010-09-07', 'Davis', 'Suwon');
+INSERT INTO bunnies VALUES (5, '2019-08-25', 'Amy', 'Seoul');
--- src/test/java/com/example/bunnylist/BunnylistApplicationTests.java
+++ src/test/java/com/example/bunnylist/BunnylistApplicationTests.java
@@ -1,39 +1,48 @@
 package com.example.bunnylist;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.List;
+
+import com.example.bunnylist.entities.Bunny;
+import com.example.bunnylist.entities.CarrotType;
 import com.example.bunnylist.repositories.BunnyRepository;
+import com.example.bunnylist.repositories.CarrotRepository;
+import com.example.bunnylist.services.BunnyService;
+import com.example.bunnylist.services.CarrotService;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BunnylistApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@BeforeAll
-	static void dataSet(){
-	}
+	@Autowired
+	BunnyRepository bunnyRepository;
+	@Autowired
+	CarrotRepository carrotRepository;
 
 	@Test
-	void bunnyServiceTest() {
-		
+	void repositoryTest() {
+		List<Bunny> bunnyList = bunnyRepository.findByTitle("Amy");
+		Bunny bunny1 = new Bunny();
+		// for (int i = 0; i < bunnyList.size(); i++) {
+		// 	if (!bunnyList.isEmpty()) {
+		// 		// assertNull(bunnyList.get(i));
+		// 		bunny1 = bunnyList.get(i);
+		// 	}
+		// }
+		assertEquals(5, bunnyList.get(0).getId(), "bunny Id");
 	}
 
 	@Test
-	void mainTest() {
-		// Auto Boxing.
-		long num = 1;
-		Long numLong = Long.valueOf(1);
-		
-		assertEquals(numLong, sayHi(num));
-
+	void carrotRepositoryTest() {
+		List<CarrotType> carrotList = carrotRepository.findCarrotTypes();
+		assertEquals("cat", carrotList.get(1).getName(), "what is the name?");
 	}
 
-	public static Long sayHi(Long number) {
-		return number;
-	}
 
 }

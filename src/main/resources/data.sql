68e23f1034e40e59acb2860a871c92cc97882fb6
DannKim57 <il0214li@daum.net>
2020-05-13 13:17:51 +0900

    updated: who feed my bunny?

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 
 
-INSERT INTO types VALUES (1, 'cat');
-INSERT INTO types VALUES (2, 'dog');
-INSERT INTO types VALUES (3, 'lizard');
-INSERT INTO types VALUES (4, 'snake');
-INSERT INTO types VALUES (5, 'bird');
-INSERT INTO types VALUES (6, 'hamster');
+INSERT INTO types VALUES (1, 'carot');
+INSERT INTO types VALUES (2, 'leaves');
+INSERT INTO types VALUES (3, 'bugs');
+INSERT INTO types VALUES (4, 'grass');
+INSERT INTO types VALUES (5, 'grasshopper');
+INSERT INTO types VALUES (6, 'flowers');
 
 
 INSERT INTO bunnies VALUES (1, '2010-09-07', 'Davis', 'Suwon');
